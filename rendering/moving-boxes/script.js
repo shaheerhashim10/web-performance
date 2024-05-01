d9b3f0b246dc78943d51a93f6aacfdbebde02afb
Shaheer Hashim <shaheerhashim10@gmail.com>
2024-05-01 16:26:22 +0500

    Changes

--- rendering/moving-boxes/script.js
+++ rendering/moving-boxes/script.js
@@ -1,14 +1,67 @@
-const {
-  containerWidth,
-  registerNextClick,
- } = setup();
+const { containerWidth, registerNextClick } = setup();
 
- const elements = Array.from(document.querySelectorAll('.element'));
+const elements = Array.from(document.querySelectorAll('.element'));
 
- registerNextClick(function (timestamp) {
+// Reading and Writing happening at the same time again and again caused reflows
+
+/* registerNextClick(function (timestamp) {
   elements.forEach((element, index) => {
     const top = element.offsetTop;
-    const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+    const nextPosition = +(
+      ((Math.sin(top + timestamp / 1000) + 1) / 2) *
+      containerWidth
+    );
     element.style.transform = `translateX(${nextPosition}px)`;
   });
- });
+}); */
+
+// Separing the Read and Write removed the Reflows
+
+/* registerNextClick(function (timestamp) {
+  const nextPositions = elements.map((element) => {
+    const top = element.offsetTop;
+    const nextPosition = +(
+      ((Math.sin(top + timestamp / 1000) + 1) / 2) *
+      containerWidth
+    );
+    return nextPosition;
+  });
+
+  elements.forEach((element, index) => {
+    const nextPosition = nextPositions[index];
+    element.style.transform = `translateX(${nextPosition}px)`;
+  });
+}); */
+
+// requestAnimationFrame makes sure that read and writes do not happen at the same time but it is not optimal
+
+/* registerNextClick(function (timestamp) {
+  elements.forEach((element, index) => {
+    const top = element.offsetTop;
+    const nextPosition = +(
+      ((Math.sin(top + timestamp / 1000) + 1) / 2) *
+      containerWidth
+    );
+
+    requestAnimationFrame(() => {
+      element.style.transform = `translateX(${nextPosition}px)`;
+    });
+  });
+}); */
+
+// Using FastDOM can solve this animation problem in which a lot of animation frames were being created
+registerNextClick(function (timestamp) {
+  elements.forEach((element, index) => {
+    fastdom.measure(() => {
+      const top = element.offsetTop;
+      const nextPosition = +(
+        ((Math.sin(top + timestamp / 1000) + 1) / 2) *
+        containerWidth
+      );
+
+      fastdom.mutate(() => {
+        element.style.transform = `translateX(${nextPosition}px)`;
+      });
+    });
+  });
+});
--- rendering/paint-storming/script.js
+++ rendering/paint-storming/script.js
@@ -1,7 +1,26 @@
-const $box = $('.box');
+/* const $box = $('.box');
 
 $box.on('click', () => {
   $box.animate({
     marginLeft: '500px',
   }, 500);
+});
+ */
+
+const box = document.querySelector('.box');
+
+box.addEventListener('mouseenter', () => {
+  box.style.willChange = 'transform';
+});
+
+box.addEventListener('transitionend', () => {
+  box.style.willChange = 'auto';
+});
+
+box.addEventListener('click', () => {
+  box.classList.toggle('move');
+});
+
+box.addEventListener('mouseleave', () => {
+  box.style.willChange = 'auto';
 });
